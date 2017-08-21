d5e8de77f6e5861f81db6b98ac45c06fe5b7a354
rcitrone <rcitrone@andrew.cmu.edu>
2017-08-21 16:24:03 -0400

    Update seasonSim.R

--- seasonSim.R
+++ seasonSim.R
@@ -2,11 +2,11 @@ source("GladiatorFunctions.R")
 library(ggthemes)
 library(ggplot2)
 library(EloRating)
-battle.log <- data.frame("weeknum" = 1, "winnerID" = 1, "loserID" = 1, "matchlevel" = 1, "seasonnum" = 1)
 
 
 
 ### Initial Player Generation ###
+battle.log <- data.frame("weeknum" = 1, "winnerID" = 1, "loserID" = 1, "matchlevel" = 1, "seasonnum" = 1)
 years <- c(16:60)
 dirty100 <- genFighter(n=210)
 dirty100 <- ageFighter(dirty100, newyear=16)
@@ -19,64 +19,89 @@ for(jj in 1:length(years)) {
 dcareer$rand <- runif(n=nrow(dcareer), min=0, max=1)
 dcareer <- dcareer[order(dcareer$rand),]
 roster <- dcareer[!duplicated(dcareer$playerID),]
+roster$born <- 50 - roster$age
 ## Placement Season
 roster$league <- sample(c(rep(1, 30), rep(2, 30), rep(3, 30), rep(4, 30), rep(5, 30), rep(6, 30), rep(7, 30)), size=nrow(roster), replace = FALSE)
 #battle.log <- data.frame("weeknum" = 1, "winnerID" = 1, "loserID" = 1, "matchlevel" = 1, "seasonnum" = 1)
 for(qq in 1:7) {
   section7 <- subset(roster, league == qq)
-  battle.log <- seasonSim(section7, battle.log = battle.log, matchlevel = qq, seasonnum = 1)
+  battle.log <- seasonSim(section7, battle.log = battle.log, matchlevel = qq, seasonnum = 50)
   print(qq)
 }
+battle.log <- battle.log[2:nrow(battle.log),]
 
-bl <- battle.log
-#Generating Standings
-bl$sumcol <- 1
-standing <- bl %>%
-  group_by(winnerID , seasonnum, matchlevel) %>%
-  summarize( wins = sum(sumcol[winnerID == winnerID],
-                        kt.titles = sum(sumcol[winnerID == winnerID & matchlevel == "Final"])))
-standing$playerID <- standing$winnerID
 #Calculating ELO
+bl <- battle.log
+bl$sumcol <- 1
 bl$order <- c(1:nrow(bl))
 elo.date <- as.Date("2017-01-01") + bl$order
-winners <- battle.log$winnerID
-losers <- battle.log$loserID
+winners <- bl$winnerID
+losers <- bl$loserID
 elo.calc <- elo.seq(winner = winners, loser = losers, Date = elo.date, init = "average", k = 65, runcheck = FALSE)
 eg <- elo.calc$cmat
 eglim <- as.data.frame(eg[nrow(eg),])
-eglim$winnerID <- rownames(eglim)
-eglim$winnerID <- as.numeric(eglim$winnerID)
-standing <- left_join(standing, eglim, by = "winnerID", copy = TRUE)
-colnames(standing) <- c("winnerID", "seasonnum", "matchlevel", "wins", "kt.wins", "ELO")
-standing <- standing[order(standing$ELO, decreasing = TRUE),]
-standing$seed <- c(1:nrow(standing))
-standing$playerID <- standing$winnerID
-roster.full <- left_join(roster, standing, by = "playerID")
+eglim$playerID <- rownames(eglim)
+eglim$playerID <- as.numeric(eglim$playerID)
+colnames(eglim) <- c("ELO", "playerID")
+roster.full <- left_join(roster, eglim, by = "playerID")
 #First Kings Tourney
-battle.log <- kingstourney(roster.full,  battle.log = battle.log , 1, 1 )
+battle.log <- kingstourney(roster.full,  battle.log = battle.log ,50, 1 )
 
-### Do it again!
-bl <- battle.log
-#Generating Standings
-bl$sumcol <- 1
-standing <- bl %>%
-  group_by(winnerID , seasonnum, matchlevel) %>%
-  summarize( wins = sum(sumcol[winnerID == winnerID],
-                        kt.titles = sum(simcol[winnerID == winnerID & matchlevel == "Final"])))
-standing$playerID <- standing$winnerID
-standing <- left_join(standing, roster, by = "playerID")
-#Calculating ELO
-bl$order <- c(1:nrow(bl))
-elo.date <- as.Date("2017-01-01") + bl$order
-winners <- battle.log$winnerID
-losers <- battle.log$loserID
-elo.calc <- elo.seq(winner = winners, loser = losers, Date = elo.date, init = "average", k = 65, runcheck = FALSE)
-eg <- elo.calc$cmat
-eglim <- as.data.frame(eg[nrow(eg),])
-eglim$winnerID <- rownames(eglim)
-eglim$winnerID <- as.numeric(eglim$winnerID)
-standing <- left_join(standing, eglim, by = "winnerID", copy = TRUE)
-colnames(standing) <- c("winnerID", "seasonnum", "matchlevel", "wins", "kt.wins", "ELO")
-standing <- standing[order(standing$ELO, decreasing = TRUE),]
-standing$seed <- c(1:nrow(standing))
-roster.full <- left_join(roster, standing, by = "playerID")
+league.seeding <- c(rep("S", 30), rep(c("A1", "A2"), 30), rep(c("B1", "B2"), 30), rep(c("J1", "J2"), 30))
+roster.full <- roster.full[order(roster.full$ELO, decreasing = TRUE),]
+roster.full$league <- league.seeding
+leaguelist <- unique(roster.full$league)
+
+runHistory <- function(df, battle.log, df.history, startyear=51, yearstorun=10, nextid=211){
+  #age fighters
+  current.id = nextid
+  for(jj in startyear:(startyear+yearstorun)){
+    # age fighters
+    df <- ageFighter(df, newyear = jj)
+    # Run Season
+    for(qq in leaguelist){
+      df.subset <- subset(df, league == qq)
+      battle.log <- seasonSim(df.subset, battle.log = battle.log, matchlevel = qq, seasonnum = jj)
+    }
+    
+    ## Calculting ELO
+    if(nrow(battle.log) > 8000){
+      startindex <- nrow(battle.log) - 8000 - 1
+      bl <- battle.log[startindex:nrow(battle.log),]
+    } else { bl <- battle.log }
+    bl$order <- c(1:nrow(bl))
+    elo.date <- as.Date("2017-01-01") + bl$order
+    winners <- bl$winnerID
+    losers <- bl$loserID
+    elo.calc <- elo.seq(winner = winners, loser = losers, Date = elo.date, init = "average", k = 50, runcheck = FALSE)
+    eg <- elo.calc$cmat
+    eglim <- as.data.frame(eg[nrow(eg),])
+    eglim$winnerID <- rownames(eglim)
+    eglim$winnerID <- as.numeric(eglim$winnerID)
+    colnames(eglim) <- c("ELO", "playerID")
+    drop <- c("ELO")
+    df <- df[,!(names(df) %in% drop)]
+    df <- left_join(df, eglim, by = "playerID")
+    
+    #Kings tourney
+    battle.log <- kingstourney(df, battle.log = battle.log, seasonnum = jj, region = 1)
+    df <- df[order(df$ELO, decreasing = TRUE),]
+    df.history <- rbind(df.history, df)
+    df$league <- leaguelist
+    df <- subset(df, (age <= 20 | !(league %in% c("J1", "J2"))) & ELO > 550)
+    numnew <- 210-nrow(df)
+    new.recruits <- genFighter(startID = current.id, n=numnew, year= jj-15)
+    current.id <- current.id + numnew
+    new.recruits <- ageFighter(new.recruits, newyear = jj)
+    num.j1 <- 30 - nrow(subset(df, league == "J1"))
+    new.league <- c(rep("J1", num.j1), rep("J2", numnew - num.j1))
+    new.recruits$league <- new.league
+    new.recruits$ELO <- 700
+    df <- rbind(df, new.recruits)
+    print(jj)
+  }
+  output <- c(df, batte.log, df.history)
+  return(output)
+}
+
+test <- runHistory(roster.full, battle.log, roster.full, startyear = 51, yearstorun = 10)
